cce61da34999c5b4554f5abddf4090749720594e
moritzfelipe <stellmacher@gmail.com>
2022-01-11 11:34:29 +0000

    Fix tests on testnet

--- test/nft-test.js
+++ test/nft-test.js
@@ -19,6 +19,7 @@ describe("MyNFT", function () {
       
       const tokenURI = "https://example.com/1"
       const tx = await myNFT.connect(account1).safeMint(account1.address, tokenURI);
+      await tx.wait();
 
       expect(await myNFT.balanceOf(account1.address)).to.equal(1);
   })
@@ -30,7 +31,9 @@ describe("MyNFT", function () {
       const tokenURI_2 = "https://example.com/1"
 
       const tx1 = await myNFT.connect(account1).safeMint(account1.address, tokenURI_1);
+      await tx1.wait();
       const tx2 = await myNFT.connect(account1).safeMint(account2.address, tokenURI_2);
+      await tx2.wait();
 
       expect(await myNFT.tokenURI(0)).to.equal(tokenURI_1);
       expect(await myNFT.tokenURI(1)).to.equal(tokenURI_2);
