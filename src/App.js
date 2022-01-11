7e2027052dba2c075b0b6d20123a823fea11ad19
moritzfelipe <stellmacher@gmail.com>
2022-01-11 19:18:06 +0000

    Move cover out of app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useContractKit } from "@celo-tools/use-contractkit";
 import React from "react";
 import Wallet from "./components/wallet/Wallet";
 import { Notification } from "./components/utils/Notifications";
+import { Cover } from "./components/utils/Cover";
 import Nfts from "./components/marketplace/Nfts";
 import coverImg from "./assets/img/sandwich.jpg";
 
@@ -44,30 +45,11 @@ const App = function AppWrapper() {
           </main>
         </Container>
       ) : (
-        <div
-          className="d-flex justify-content-center flex-column text-center "
-          style={{ background: "#000", minHeight: "100vh" }}
-        >
-          <div className="mt-auto text-light mb-5">
-            <div
-              className=" ratio ratio-1x1 mx-auto mb-2"
-              style={{ maxWidth: "320px" }}
-            >
-              <img src={coverImg} alt="" />
-            </div>
-            <h1>NFT Marketplace</h1>
-            <p>Please connect your wallet to continue.</p>
-            <Button
-              onClick={() => connect().catch((e) => console.log(e))}
-              variant="outline-light"
-              className="rounded-pill px-3 mt-3"
-            >
-              Connect Wallet
-            </Button>
-          </div>
-
-          <p className="mt-auto text-secondary">Powered by Celo</p>
-        </div>
+        <Cover
+          name="NFT Marketplace"
+          coverImg={coverImg}
+          connect={connect}
+        />
       )}
     </>
   );
