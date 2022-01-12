20583a2e3d361f25db5d087d6a36adca668c5629
moritzfelipe <stellmacher@gmail.com>
2022-01-12 12:46:39 +0000

    Cleanup

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,11 +2,9 @@ import BigNumber from 'bignumber.js';
 import { ERC20_DECIMALS } from './constants';
 
 export const truncateAddress = (address) => {
-    console.log({address})
     if(!address) return
-  return  address.slice(0, 5) + "..." + address.slice(address.length - 4, address.length);
-
-}//`${address.slice(0, 6)}…${address.slice(-6)}`;
+    return  address.slice(0, 5) + "..." + address.slice(address.length - 4, address.length);
+}
 
 export const weiToCusd = (amount) => new BigNumber(amount).shiftedBy(-ERC20_DECIMALS).toString();
 
